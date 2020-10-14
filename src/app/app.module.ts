8e8aab0955ffaa438fa9b4d23f3b4fa71dee52e1
Venkata Siddhartha Sayala <siddhartha@MacBook-Pro.local>
2020-10-14 23:48:57 +0530

    testing component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { FooterComponent } from './common/footer/footer.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { TestingComponentComponent } from './testing-component/testing-component.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    TestingComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/testing-component/testing-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testing-component',
+  templateUrl: './testing-component.component.html',
+  styleUrls: ['./testing-component.component.css']
+})
+export class TestingComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
