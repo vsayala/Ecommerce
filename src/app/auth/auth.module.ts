22b1614c7e87496ff3bf6f58d017360ced6e6760
Venkata Siddhartha Sayala <siddhartha@MacBook-Pro.local>
2020-10-20 13:00:40 +0530

    products_module_lazyloading

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { SendMailComponent } from './send-mail/send-mail.component';
 import { UpdatePasswordComponent } from './update-password/update-password.component';
-import { RegisterComponent } from './register/register.component';
 
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'send-mail', component: SendMailComponent},
+  {path: 'update-password', component: UpdatePasswordComponent}
+];
 
 
 @NgModule({
-  declarations: [LoginComponent, SendMailComponent, UpdatePasswordComponent, RegisterComponent],
+  declarations: [
+    LoginComponent,
+    SendMailComponent,
+    UpdatePasswordComponent,
+    RegisterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes, {useHash: true})
   ]
 })
 export class AuthModule { }
